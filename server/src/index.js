cb6cad26a6139960fefd4ab2e8d036218336c03a
Teddy Odhiambo <tedb19@gmail.com>
2020-06-16 14:57:39 +0300

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import server from './config/apollo'
+import app from './config/app'
+
+const port = process.env.PORT
+
+server.applyMiddleware({ app })
+
+app.listen({ port }, () => {
+    console.log(`Server ready @ http://localhost:${port}${server.graphqlPath}`)
+})
